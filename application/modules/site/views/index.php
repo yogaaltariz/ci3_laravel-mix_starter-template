a74bc07d959aca7bd809cd8c05b2dd2bdcd9fc25
yogaaltariz <yoga.altariz@gmail.com>
2020-10-08 14:19:47 +0700

    change header

--- application/modules/site/views/index.php
+++ application/modules/site/views/index.php
@@ -1,27 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+<div class="p-8">
+    <div x-data="{ show: true }">
+        <h1 class="font-bold text-6xl" x-show="show">Hello World</h1>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="p-8">
-        <div x-data="{ show: true }">
-            <h1 class="font-bold text-6xl" x-show="show">Hello World</h1>
-
-            <button class="mt-2 px-4 py-2 text-white bg-blue-600 shadow-lg rounded-lg" @click="show = !show">
-                <template x-if="show">
-                    <span>hidden</span>
-                </template>
-                <template x-if="!show">
-                    <span>show</span>
-                </template>
-            </button>
-        </div>
+        <button class="mt-2 px-4 py-2 text-white bg-blue-600 shadow-lg rounded-lg" @click="show = !show">
+            <template x-if="show">
+                <span>hidden</span>
+            </template>
+            <template x-if="!show">
+                <span>show</span>
+            </template>
+        </button>
     </div>
-</body>
-
-</html>
+</div>
--- application/modules/site/views/partial/header.php
+++ application/modules/site/views/partial/header.php
@@ -4,12 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="og:site_name" content="portal kampus" />
-    <meta property="og:title" content="Portal Kampus" />
-    <meta property="og:description" content="Deskripsi portal kampus" />
-
     <link rel="stylesheet" href="<?= base_url('assets/css/app.css') ?>">
-    <title>Portal Kampus</title>
+    <title>Codeigniter 3 HMVC - Laravel Mix</title>
 </head>
 
 <body>
